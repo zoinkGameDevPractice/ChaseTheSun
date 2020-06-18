4a70fe36ac93f85d0b81f1c651895d82722dd17f
zoinkGameDevPractice <zoinkGameDevPractice@users.noreply.github.com>
2020-06-17 20:05:45 -0400

    Added score system

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Rigidbody2D player;
+    public TextMeshProUGUI text;
+
+    Vector2 initialPos;
+    int meters;
+    int topMeters;
+
+    void Start()
+    {
+        initialPos = player.transform.position;
+    }
+
+    void Update()
+    {
+        CalculateMeters();
+    }
+
+    void CalculateMeters()
+    {
+        meters = Mathf.RoundToInt(Vector2.Distance(new Vector2(0, player.transform.position.y), new Vector2(0, initialPos.y)));
+        if (topMeters <= meters)
+        {
+            topMeters = meters;
+        }
+        Display(topMeters);
+    }
+
+    void Display(int i)
+    {
+        text.text = i.ToString() + "M";
+    }
+}
