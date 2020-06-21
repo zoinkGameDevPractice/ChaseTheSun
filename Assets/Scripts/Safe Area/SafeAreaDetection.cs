0d4a84ec3294856a4f17f0b0879593cf1425952d
zoinkGameDevPractice <zoinkGameDevPractice@users.noreply.github.com>
2020-06-20 23:50:38 -0400

    Fixed notch issue

---
+++ Assets/Scripts/Safe Area/SafeAreaDetection.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SafeAreaDetection : MonoBehaviour
+{
+    public delegate void SafeAreaChanged(Rect safeArea);
+    public static event SafeAreaChanged OnSafeAreaChanged;
+
+    Rect safeArea;
+
+    void Awake()
+    {
+        safeArea = Screen.safeArea;
+    }
+
+    void Update()
+    {
+        if(safeArea != Screen.safeArea)
+        {
+            safeArea = Screen.safeArea;
+            OnSafeAreaChanged?.Invoke(safeArea);
+        }
+    }
+}
